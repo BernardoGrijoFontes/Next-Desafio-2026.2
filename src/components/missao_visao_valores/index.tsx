9436722681e5f2087f71e19af73800bac4f8b48a
Bernardo Grijó Fontes <b3rnardogfontes@gmail.com>
2026-08-02 16:43:22 -0300

    feat: imagens na seção sobre nós agora possuem um fundo na cor principal do site

--- src/components/missao_visao_valores/index.tsx
+++ src/components/missao_visao_valores/index.tsx
@@ -9,22 +9,26 @@ export default function Missao(){
                     <h1 className="text-[150%] font-extrabold text-[#6271CF]">Missão</h1>
                     <p className="text-black w-full">Na Next Wear Brechó, nossa missão é tornar a moda acessível, consciente e cheia de personalidade.</p>
                 </div>
-                <Image
-                src={'/assets/Pessoas-felizes.jpg'}
-                alt="Pessoas felizes que simbolizam a satisfação dos clientes"
-                height={300}
-                width={300}
-                className="rounded-full"
-                />
+                <div className="rounded-full bg-[#6271CF]">
+                    <Image
+                    src={'/assets/Pessoas-felizes.jpg'}
+                    alt="Pessoas felizes que simbolizam a satisfação dos clientes"
+                    height={300}
+                    width={300}
+                    className="rounded-full opacity-20"
+                    />
+                </div>
             </div>
             <div className="flex flex-row w-full justify-around">
-                <Image
-                src={'/assets/56759557-desktop-wallpaper-4k-wallpaper.jpg'}
-                alt="Pessoas felizes que simbolizam a satisfação dos clientes"
-                height={300}
-                width={300}
-                className="rounded-full"
-                />
+                <div className="rounded-full bg-[#6271CF]">
+                    <Image
+                    src={'/assets/56759557-desktop-wallpaper-4k-wallpaper.jpg'}
+                    alt="Pessoas felizes que simbolizam a satisfação dos clientes"
+                    height={300}
+                    width={300}
+                    className="rounded-full opacity-20"
+                    />
+                </div>
                 <div className="flex flex-col gap-4 items-center justify-center text-center max-w-xl">
                     <h1 className="text-[150%] font-extrabold text-[#6271CF]">Visão</h1>
                     <p className="text-black w-full">Queremos inspirar cada vez mais pessoas a transformar a forma como consomem moda, promovendo um futuro em que qualidade, economia e respeito ao meio ambiente caminhem juntos.</p>
@@ -35,13 +39,15 @@ export default function Missao(){
                     <h1 className="text-[150%] font-extrabold text-[#6271CF]">Valores</h1>
                     <p className="text-black w-full">Valorizamos a sustentabilidade, a autenticidade e o respeito ao consumidor. Trabalhamos com transparência, oferecendo peças cuidadosamente selecionadas, preços justos e um atendimento acolhedor.</p>
                 </div>
-                <Image
-                src={'/assets/arara_de_roupas_ypedia-scaled.jpg'}
-                alt="Pessoas felizes que simbolizam a satisfação dos clientes"
-                height={300}
-                width={300}
-                className="rounded-full"
-                />
+                <div className="rounded-full bg-[#6271CF]">
+                    <Image
+                    src={'/assets/arara_de_roupas_ypedia-scaled.jpg'}
+                    alt="Pessoas felizes que simbolizam a satisfação dos clientes"
+                    height={300}
+                    width={300}
+                    className="rounded-full opacity-20"
+                    />
+                </div>
             </div>
         </div>
     )
