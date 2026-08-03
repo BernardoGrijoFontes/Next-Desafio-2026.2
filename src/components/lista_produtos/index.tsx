4efda5a401d22ffd9f20f4017c14247ec9856021
Bernardo Grijó Fontes <b3rnardogfontes@gmail.com>
2026-08-03 00:12:22 -0300

    feat: visual da página de produtos implementado

--- src/components/lista_produtos/index.tsx
+++ src/components/lista_produtos/index.tsx
@@ -42,29 +42,80 @@ const produtos = [
         preco: "R$ 270,00",
         descricao: "Descrição",
         imagem: "/assets/Logo_NW.png",
-    }
+    },
+    {
+        id: 7,
+        nome: "Produto 7",
+        preco: "R$ 270,00",
+        descricao: "Descrição",
+        imagem: "/assets/Logo_NW.png",
+    },
+    {
+        id: 8,
+        nome: "Produto 7",
+        preco: "R$ 270,00",
+        descricao: "Descrição",
+        imagem: "/assets/Logo_NW.png",
+    },
+    {
+        id: 9,
+        nome: "Produto 7",
+        preco: "R$ 270,00",
+        descricao: "Descrição",
+        imagem: "/assets/Logo_NW.png",
+    },
+    {
+        id: 10,
+        nome: "Produto 7",
+        preco: "R$ 270,00",
+        descricao: "Descrição",
+        imagem: "/assets/Logo_NW.png",
+    },
+    {
+        id: 11,
+        nome: "Produto 7",
+        preco: "R$ 270,00",
+        descricao: "Descrição",
+        imagem: "/assets/Logo_NW.png",
+    },
+    {
+        id: 12,
+        nome: "Produto 7",
+        preco: "R$ 270,00",
+        descricao: "Descrição",
+        imagem: "/assets/Logo_NW.png",
+    },
+    {
+        id: 13,
+        nome: "Produto 7",
+        preco: "R$ 270,00",
+        descricao: "Descrição",
+        imagem: "/assets/Logo_NW.png",
+    },
 ];
 
 export default function ListaDeProdutos(){
     return (
-        <div className="flex flex-col items-center justify-center py-8 px-4">
-            <h1 className="text-[200%] font-extrabold text-[#6271CF]">Mais vendidos</h1>
-            {produtos.map((produto) => (
-                <div key={produto.id} className="bg-white flex flex-col py-4 px-4 items-center justify-center text-black h-full gap-2 rounded-lg w-[80%] border-2 border-[#6271CF] hover:scale-105 cursor-pointer duration-75 ease-in-out">
-                    <Image 
-                    src={produto.imagem}
-                    alt={produto.nome}
-                    width={200}
-                    height={200}
-                    />
-                    <div className="flex flex-col items-center justify-center gap-4 w-full">
-                        <h1 className="text-[100%] font-extrabold">{produto.nome}</h1>
-                        <h2 className="text-[100%] font-extrabold">{produto.preco}</h2>
-                        <h1 className="text-[100%] text-black/50 font-extrabold overflow-hidden">{produto.descricao}</h1>
-                        <button className="text-[100%] text-white bg-[#6271CF] font-extrabold py-2 px-2 rounded-lg w-full hover:scale-105 cursor-pointer duration-75 ease-in-out">Ver mais</button>
-                    </div>
-                </div> 
-            ))}
+        <div className="flex flex-col items-center justify-center py-8 px-20 gap-8">
+            <h1 className="text-[200%] font-extrabold text-[#6271CF]">Produtos</h1>
+            <div className="grid grid-cols-4 w-full gap-16">
+                {produtos.map((produto) => (
+                    <div key={produto.id} className="bg-white flex flex-col py-4 px-4 items-center justify-center text-black h-full gap-2 rounded-lg border-2 border-[#6271CF] hover:scale-105 cursor-pointer duration-75 ease-in-out">
+                        <Image 
+                        src={produto.imagem}
+                        alt={produto.nome}
+                        width={200}
+                        height={200}
+                        />
+                        <div className="flex flex-col items-center justify-center gap-4 w-full">
+                            <h1 className="text-[100%] font-extrabold">{produto.nome}</h1>
+                            <h2 className="text-[100%] font-extrabold">{produto.preco}</h2>
+                            <h1 className="text-[100%] text-black/50 font-extrabold overflow-hidden">{produto.descricao}</h1>
+                            <button className="text-[100%] text-white bg-[#6271CF] font-extrabold py-2 px-2 rounded-lg w-full hover:scale-105 cursor-pointer duration-75 ease-in-out">Ver mais</button>
+                        </div>
+                    </div> 
+                ))}
+            </div>
         </div>
     )
 }
