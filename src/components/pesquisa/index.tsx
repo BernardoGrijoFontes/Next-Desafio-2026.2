0628a525109e85f647337a1f7d62d9373e92837c
Bernardo Grijó Fontes <b3rnardogfontes@gmail.com>
2026-08-23 17:52:23 -0300

    fix: correção e estilização da barra de pesquisa

--- src/components/pesquisa/index.tsx
+++ src/components/pesquisa/index.tsx
@@ -7,36 +7,37 @@ type PesquisaProps = {
 };
 
 export default function Pesquisa({ onSearch }: PesquisaProps) {
-    const [valor, setValor] = useState("");
+  const [hover, setHover] = useState(false)
+  const [valor, setValor] = useState("");
 
-    function handleSubmit(e: React.FormEvent) {
-        e.preventDefault();
-        onSearch(valor);    
-    }
+  function handleSubmit(e: React.FormEvent) {
+      e.preventDefault();
+      onSearch(valor);    
+  }
 
   return (
     <form
-      className="flex basis-full px-20 w-full"
+      className="flex basis-full px-20 w-full justify-center outline-none"
       autoComplete="off"
       onSubmit={handleSubmit}
     >
-      <div className="flex w-full flex-col items-center gap-6 rounded-lg border-2 border-black/15">
-        <div className="relative flex flex-row w-full items-center rounded-lg"> 
-          <SearchIcon className="w-7 h-7 absolute text-black left-4"/>
+      <div onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)} className={`flex flex-col h-fit items-center gap-6 rounded-full border-2 border-black/15 transition-all duration-300 ${hover ? "w-[60%]" : "w-[30%]"}`}>
+        <div className="outline-none py-2 relative flex flex-row w-full items-center rounded-lg px-4"> 
           <input
             id="search"
             name="search"
             type="text"
-            className="w-full px-16 py-6"
+            className="outline-none w-full"
             placeholder="Faça sua busca"
             value={valor}
             onChange={e => setValor(e.target.value)}
           /> 
           <button
             type="submit"
-            className="ml-4 px-4 py-2 bg-blue-500 text-white rounded"
+            className="ml-4 px-4 py-2 bg-black/25 text-white rounded-full hover:scale-105 cursor-pointer ease-in-out duration-75"
           >
-            Buscar
+            <SearchIcon/>
           </button>
         </div>
       </div>
